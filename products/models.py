bc8cd2dcbfefb23afd473040e340c72f40cb711b
ibrahimhus7ein <ibrahimhussein.x@gmail.com>
2022-10-02 00:34:01 +0300

    product

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to = "static/products")
     price = models.CharField(max_length=20)
     description = models.TextField()
+    favourite  = models.BooleanField(default=False)
 
     def __str__(self):
         return self.product_name
